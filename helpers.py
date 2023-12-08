12b396b078f808e16a2905436dfaf62c7c00ca21
Shubham Jalan <subham.jalan@plaksha.edu.in>
2023-12-09 00:19:42 +0530

    added helpers

---
+++ helpers.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+def verify_duties(duties_data, wifi_data):
+    pass
--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 import time
 from fastapi import FastAPI
 from apscheduler.schedulers.background import BackgroundScheduler
-
+import pandas as pd
+import helpers
 # load .env files
 load_dotenv()
 
@@ -17,16 +18,25 @@ supabase: Client = create_client(url, key)
 app = FastAPI()
 
 
-# process data function
-def process_data():
-    print("Yo")
-
 # # schedule process_data to run every 60 seconds
 # scheduler = BackgroundScheduler()
 # scheduler.add_job(process_data, 'interval', seconds=60)
 # scheduler.start()
 
 # get excel sheet
-@app.get("/excel")
-def get_excel():
-    return {"Hello": "World"}
+# @app.get("/excel")
+# def get_excel():
+#     # patrols = supabase.table('tasks').select("*, personnel_list(rank)").eq('task_type', 'Patrol').execute().data
+#     # return patrols
+#     return {"data": "i am an excel sheet"}
+
+# @app.get("/")
+# def show_nothing():
+#     return "I am showing nothing"
+
+@app.get("/verify")
+def verify_patrols():
+    uncompleted_duties = supabase.table('tasks').select("*, personnel_list(guard_phone)").neq('task_type', 'Patrol').is_('timecompletion', "null").execute().data
+    wifi_conn = supabase.table('wifi_connections').select("*").execute().data
+    response = helpers.verify_duties(uncompleted_duties, wifi_conn)
+    return wifi_conn
